44328cd2c25ab5ebc122d0534584c1bb65a07654
maher <gomemahero.com>
2022-07-27 04:04:22 +0300

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { checkCookies, getCookie, getCookies } from "cookies-next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -12,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <a href="/api/google">Login with Google</a>
+      <Link href="/api/google">Login with Google</Link>
     </div>
   );
 }
